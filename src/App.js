0b39e2c5ef93933c7738e7f4d68f434ec879386e
mahtaz <mahta.zarean@gmail.com>
2023-03-18 21:57:37 +0330

    adding github link

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ function App() {
       <header className="App-header">
         <h1>Weather App</h1>
         <Search />
+        open sourced on{" "}
+        <a href="https://github.com/mahtaz/react-weather-app">github</a>
       </header>
     </div>
   );
